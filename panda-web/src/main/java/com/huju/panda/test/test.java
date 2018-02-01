66fecef32c95613081407afdd4e100792079002e
huju <v_huju@baidu.com>
2018-02-01 22:41:51 +0800

    清除不需要提交的，并设置不需要提交的文件

---
+++ panda-web/src/main/java/com/huju/panda/test/test.java
@@ -0,0 +1,10 @@
+package com.huju.panda.test;
+
+/**
+ * Created by Administrator on 2018/2/1.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈。。。");
+    }
+}
