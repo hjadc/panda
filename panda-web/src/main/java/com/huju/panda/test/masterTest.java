c28be8a0cf23f17c8c871939f12a637ac5bd659b
huju <v_huju@baidu.com>
2018-02-02 10:34:30 +0800

    测试文件A

---
+++ panda-web/src/main/java/com/huju/panda/test/masterTest.java
@@ -0,0 +1,11 @@
+package com.huju.panda.test;
+
+/**
+ * Created by v_huju on 2018/2/2.
+ */
+public class masterTest {
+
+    public static void main(String[] args) {
+        System.out.println("这是master分支的测试文件");
+    }
+}
